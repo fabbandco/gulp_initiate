68f0430efe1914095a4da8d7eb164f1111b99cb7
Fabien <dev@linktogo.fr>
2016-01-06 22:13:13 +0100

    initiate project

---
+++ src/app/index.constants.js
@@ -0,0 +1,10 @@
+/* global malarkey:false, moment:false */
+(function() {
+  'use strict';
+
+  angular
+    .module('gulpInitiate')
+    .constant('malarkey', malarkey)
+    .constant('moment', moment);
+
+})();
